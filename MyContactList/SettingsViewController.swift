0ba915387e8852e0b95ab6f518ad9927e1a9306c
Mr-GR <manny2144@icloud.com>
2024-03-21 18:25:12 -0400

    "Added Sort order and view picker"

--- MyContactList/SettingsViewController.swift
+++ MyContactList/SettingsViewController.swift
@@ -7,23 +7,44 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+   
 
+    @IBOutlet weak var pckSortField: UIPickerView!
+    @IBOutlet weak var swAcending: UISwitch!
+    
+    let sortOrderItems: Array<String> = ["Contact Name", "City", "Birthday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        pckSortField.dataSource = self;
+        pckSortField.delegate = self;
+    }
+    
+    @IBAction func sortDirectionChanged(_ sender: Any) {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return sortOrderItems[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Chosen item: \(sortOrderItems[row])")
+    }
 
 }
