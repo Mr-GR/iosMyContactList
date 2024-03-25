891af87b578e23a1b0b35c1dc738cb4a78cee9df
Mr-GR <manny2144@icloud.com>
2024-03-25 14:15:26 -0400

    "Added Global Constants"

---
+++ MyContactList/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MyContactList
+//
+//  Created by Manuel Guevara Reyes on 3/25/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+}
--- MyContactList/SettingsViewController.swift
+++ MyContactList/SettingsViewController.swift
@@ -9,12 +9,7 @@ import UIKit
 
 class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
     
-   
-
     @IBOutlet weak var pckSortField: UIPickerView!
     @IBOutlet weak var swAcending: UISwitch!
     
@@ -29,10 +24,31 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         pckSortField.delegate = self;
     }
     
-    @IBAction func sortDirectionChanged(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        let settings = UserDefaults.standard
+        swAcending.setOn(settings.bool(forKey: Constants.kSortDirectionAscending), animated: true)
+        let sortField = settings.string(forKey: Constants.kSortField)
+        var i = 0
+        for field in sortOrderItems {
+            if field == sortField {
+                pckSortField.selectRow(i, inComponent: 0, animated: false)
+            }
+            i += 1
+        }
+        pckSortField.reloadComponent(0)
     }
     
-
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+        let settings = UserDefaults.standard
+        settings.set(swAcending.isOn, forKey: Constants.kSortDirectionAscending)
+        settings.synchronize()
+    }
+    
+    // Mark: UIPickerViewDelegate methods 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return sortOrderItems.count
@@ -44,6 +60,10 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let sortFiled = sortOrderItems[row]
+        let settings = UserDefaults.standard
+        settings.setValue(sortFiled, forKey: Constants.kSortField)
+        settings.synchronize()
         print("Chosen item: \(sortOrderItems[row])")
     }
 
